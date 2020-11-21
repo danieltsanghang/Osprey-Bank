d042a0ccc33cc66913769656cd415a698cc71935
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2020-11-21 21:13:23 +0000

    Create input validation and authentication for user login
    Created the input validation and authentication using ruby's 'bcrypt' gem. Renamed the "password"
    column in the users table in the DB to "password_digest" in line with what bcrypt needs for
    authentication. Created tests for user object creation (i.e. password, email, username).

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,27 @@
 class User < ApplicationRecord
+    has_secure_password # Adds password functionality for each User
+
+    # User validation
+    validates :username, presence: true,
+                         length: { minimum: 6, maximum: 20 },
+                         uniqueness: { case_sensitive: false }
+
+    validates :password, presence: true,
+                         length: { minimum: 8, maximum: 30 }
+
+    validates :email, presence: true, 
+                      format: { with: URI::MailTo::EMAIL_REGEXP }, 
+                      uniqueness: { case_sensitive: false }
+
+    before_save :downcase_username
+
     has_many :accounts
+
+    private
+
+        # Method that makes the username all lower case before saving to the database
+        def downcase_username() 
+            self.username = username.downcase
+        end
+        
 end
