2e5246ee2cf2b518d2651353ee3077e70ed0bbee
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2020-11-22 23:59:43 +0000

    Add comments
    Completed user login authentication, however, pages can still be opened by
    unauthorised users through typing the correct URL. This security concern will
    be pushed back for later, until the appropriate skills have been learnt.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,18 +1,23 @@
 module SessionsHelper
+
+    # Log the user in and assign the session user_id to the user's ID
     def login(user)
         session[:user_id] = user.id
     end
 
+    # Find the current user
     def current_user
         if session[:user_id]
             User.find_by(id: session[:user_id])
         end
     end
 
+    # Checks if user is logged in
     def logged_in?
         !current_user.nil?
     end
 
+    # Logs the user out by terminating the session
     def logout
         session.delete(:user_id)
     end
