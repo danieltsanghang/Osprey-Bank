e78c07feb50d053c99298785c7774cd74b10737f
DanielTsang <danieltsang00@gmail.com>
2020-11-24 19:17:29 +0000

    added relavent comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+# allow user data to be pulled for user/show
   resources :users
 end
--- db/migrate/20201124183153_add_details_to_users.rb
+++ db/migrate/20201124183153_add_details_to_users.rb
@@ -1,4 +1,5 @@
 class AddDetailsToUsers < ActiveRecord::Migration[6.0]
+  # add additional fields to the users table
   def change
     add_column :users, :DOB, :Date
     add_column :users, :phoneNumber, :int
