359e763ff67d53c29eaa8390ee7dfbde3c952f5f
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2020-11-28 17:43:31 +0000

    Test Transaction History
    Tested the transaction history functionality. This required some helper methods added to the test_helper, and a range
    of new test fixtures.

--- test/controllers/transactions_controller_test.rb
+++ test/controllers/transactions_controller_test.rb
@@ -6,4 +6,39 @@ class TransactionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :redirect
   end
 
+  test 'should open transactions page after login' do
+    # Get user from fixtures and login
+    user = users(:user)
+    login_as_user(user, "password1")
+    assert_response :redirect # User redirected after authenticated
+
+    get transactions_url
+    assert_response :success
+  end
+
+  test 'should open account transactions page after login' do
+    # Get user from fixtures and login
+    user = users(:user_2)
+    login_as_user(user, "password2")
+    assert_response :redirect # User redirected after authenticated
+
+    # Get one of the accounts associated with a user, and view it's transactions
+    account = user.accounts[0]
+    get account_transactions_url(account)
+    assert_response :success
+  end
+
+  test 'should not open account transactions page after login for another user' do
+    # Get user from fixtures and login
+    user = users(:user_2)
+    not_user = users(:user)
+    login_as_user(user, "password2")
+    assert_response :redirect # User redirected after authenticated
+
+    # Get one of the accounts associated with a user, and view it's transactions
+    account = not_user.accounts[0]
+    get account_transactions_url(account)
+    assert_response 404
+  end
+
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,13 +1,15 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
 
   test "should show user" do
+    # Get user from fixtures and login
     user = users(:user)
+    login_as_user(user, "password1")
+    assert_response :redirect # User redirected after authenticated
+
+    # Show user profile
     get user_url(user)
-    assert_response:success
+    assert_response :success
   end
 end
