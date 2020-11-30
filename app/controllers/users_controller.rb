3b8b966b09464bfda349c0cd656b650fda6c5fa1
Gila <gilapearlman@gmail.com>
2020-11-30 15:49:28 +0000

    added new controllers for edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
 
-  before_action :redirect_to_login_if_not_logged_in
+  #before_action :redirect_to_login_if_not_logged_in
 
   def show
     @user = User.find(params[:id])
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
+
+  def update
+
+  end  
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create four Users for testing
+User.create(fname: "Kate", lname: "Hudgeson", email: "kateh@gmail.com", username: "khudgeson", password: "Test12345", password_confirmation: "Test12345", isAdmin: false)
+User.create(fname: "John", lname: "Smith", email: "jsmith@gmail.com", username: "jsmith", password: "Test12345", password_confirmation: "Test12345", isAdmin: false)
+User.create(fname: "Sam", lname: "Collins", email: "samcollins@gmail.com", username: "scollins", password: "Test12345", password_confirmation: "Test12345", isAdmin: false)
+User.create(fname: "Mark", lname: "Amber", email: "markamber@gmail.com", username: "mamber", password: "Test12345", password_confirmation: "Test12345", isAdmin: false)
