76f596ff6bddd7cc5a0583d6dc7f5a61e7edcc73
Jonathan Rivera <jonathan.rivera@kcl.ac.uk>
2020-11-30 16:48:48 +0000

    Implement AccountsController and update ApplicationHelper
    Implemented show in AccountsController and created redirect_to_404 method in ApplicationHelper

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,8 +1,26 @@
 class AccountsController < ApplicationController
 
   #before_action :redirect_to_login_if_not_logged_in
+  before_action :redirect_to_404_if_not_authorized
 
-  
   def show
+    @accounts = Account.where("user_id = ?", params[:id]) #obtain array of accounts
   end
+
+
+
+  private
+
+      def redirect_to_404_if_not_authorized
+        redirect_to_login_if_not_logged_in
+
+        unless(params.has_key?(:id))
+          redirect_to_404
+        else
+          unless(User.exists?(params[:id]) && User.find(params[:id]).id == current_user.id)
+            redirect_to_404
+          end
+        end
+      end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
-    include SessionsHelper # Add session helpers to all controllers so they can use helper methods like login, logout, etc. 
+    include SessionsHelper # Add session helpers to all controllers so they can use helper methods like login, logout, etc.
+    include ApplicationHelper # Add helpers applicable to all controllers
+
 end
