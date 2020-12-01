fc649a54be42149537e2679c3cfb8c1e87ac2ac8
Jonathan Rivera <jonathan.rivera@kcl.ac.uk>
2020-12-01 01:17:30 +0000

    Add Accounts Model tests

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "sort code has 6 digits" do
+    account = Account.new(:user_id => 1, :sortCode => 123456, :accountNumber => 12345678)
+    assert account.valid?
+  end
+
+  test "sort code does not have 6 digits" do
+    account = Account.new(:user_id => 1, :sortCode => 12345, :accountNumber => 12345678)
+    assert_not account.valid?
+  end
+
+  test "account number is within range 8..9" do
+    account = Account.new(:user_id => 1, :sortCode => 123456, :accountNumber => 12345678)
+    assert account.valid?
+  end
+
+  test "account number is not within range 8..9" do
+    account = Account.new(:user_id => 1, :sortCode => 123456, :accountNumber => 1234567)
+    assert_not account.valid?
+  end
+
+  
 end
