0cb9d369f22623a6b9108f90b5b1b79ff7a60431
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2020-12-03 15:44:35 +0000

    Improve UI
    Certain UI changes were made that made the web application have better
    resourceful routing by removing more than one resource in a single page.
    Also, some formatting changes were made.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :redirect_to_login_if_not_logged_in
+  before_action :redirect_to_404_if_not_authorized
 
   def show
     @user = User.find(params[:id])
@@ -21,8 +22,17 @@ class UsersController < ApplicationController
 
   private
 
+  # Sanitise inputs
     def user_params
       params.require(:user).permit(:fname, :lname, :username, :email, :phoneNumber, :address)
+    end
+
+    # Security check to make sure the user is accessing only their users page
+    def redirect_to_404_if_not_authorized
+      if(current_user.id !=  params[:id].to_i)
+        redirect_to_404
+        return
+      end
     end
 
 end
