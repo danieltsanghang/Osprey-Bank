14c4c51ea15e01c996f908cced40a84198328aac
DanielTsang <danieltsang00@gmail.com>
2020-12-11 21:13:17 +0000

    complete helper functions for generating users, accounts, and transactions

--- app/helpers/admin/generator_helper.rb
+++ app/helpers/admin/generator_helper.rb
@@ -3,7 +3,7 @@ module Admin::GeneratorHelper
 def generateUsers(amount)
   (User.all.size .. User.all.size + amount).each do |id|
       User.create!(
-          id: id, # each user is assigned an id from 1-20
+          # id: id, # each user is assigned an id from 1-20
           fname: Faker::Name.unique.first_name,
           lname: Faker::Name.unique.last_name ,
           email: Faker::Internet.email,
@@ -22,7 +22,7 @@ def generateAccounts(amount, range)
   (Account.all.size .. Account.all.size + amount).each do |id|
       Account.create!(
           id: id,
-          user_id: rand(1..20),
+          user_id: rand(User.all.size - range .. User.all.size),
           sortCode: Faker::Number.number(digits: 6),
           accountNumber: Faker::Number.number(digits: 8),
           balance: Faker::Number.number(digits: 7),
@@ -30,5 +30,24 @@ def generateAccounts(amount, range)
       )
   end
 end
+
+def generateTransactions(amount, range)
+  (Transaction.all.size .. Transaction.all.size + amount).each do |id|
+      Transaction.create!(
+        sender_id: Account.find(rand(Account.all.size - range .. Account.all.size)).id,
+        receiver_id: Account.find(rand(0 .. Account.all.size - range)).id,
+        amount: Faker::Number.number(digits: 4),
+        timeStamp: Faker::Date.backward(days: 100)
+      )
+      Transaction.create!(
+        sender_id: Account.find(rand(0 .. Account.all.size - range)).id,
+        receiver_id: Account.find(rand(Account.all.size - range .. Account.all.size)).id,
+        amount: Faker::Number.number(digits: 4),
+        timeStamp: Faker::Date.backward(days: 100)
+      )
+    end
+end
+
+
 
 end
