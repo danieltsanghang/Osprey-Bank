adc12587500bca07b4c021cbcf6e47b05058ac1e
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2020-12-13 00:47:01 +0000

    Fix account model tests to use a currency when an account object is created
    The model validation applies a default currency and balance before saving to the DB (for Account objects), and not when creating objects. Hence, in the test file, the created
    objects are always invalid and fail the tests.

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -2,22 +2,22 @@ require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
   test "sort code has 6 digits" do
-    account = Account.new(:user_id => 1, :sortCode => 123456, :accountNumber => 12345678)
+    account = Account.new(:user_id => 1, :sortCode => 123456, :accountNumber => 12345678, :currency => "USD")
     assert account.valid?
   end
 
   test "sort code does not have 6 digits" do
-    account = Account.new(:user_id => 1, :sortCode => 12345, :accountNumber => 12345678)
+    account = Account.new(:user_id => 1, :sortCode => 12345, :accountNumber => 12345678, :currency => "USD")
     assert_not account.valid?
   end
 
   test "account number is within range 8..9" do
-    account = Account.new(:user_id => 1, :sortCode => 123456, :accountNumber => 12345678)
+    account = Account.new(:user_id => 1, :sortCode => 123456, :accountNumber => 12345678, :currency => "USD")
     assert account.valid?
   end
 
   test "account number is not within range 8..9" do
-    account = Account.new(:user_id => 1, :sortCode => 123456, :accountNumber => 1234567)
+    account = Account.new(:user_id => 1, :sortCode => 123456, :accountNumber => 1234567, :currency => "USD")
     assert_not account.valid?
   end
 
