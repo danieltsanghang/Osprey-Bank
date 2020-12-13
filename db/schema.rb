da1c77ea5f1ae4ee1bf6eba5ad0d78a6b94ad132
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2020-12-13 17:37:06 +0000

    Auto increment account id from 1000

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_001321) do
+ActiveRecord::Schema.define(version: 2020_12_13_162129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,19 +19,19 @@ ActiveRecord::Schema.define(version: 2020_12_07_001321) do
     t.integer "user_id"
     t.integer "sortCode"
     t.integer "accountNumber"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.decimal "balance"
     t.string "currency"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "transactions", force: :cascade do |t|
     t.integer "sender_id"
     t.integer "receiver_id"
+    t.decimal "amount"
     t.date "timeStamp"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.decimal "amount"
   end
 
   create_table "users", force: :cascade do |t|
