388097393110531e276fabaea49dbc2b432664b5
Jonathan Rivera <jonathan.rivera@kcl.ac.uk>
2020-12-15 16:51:26 +0000

    Minor test error fix

--- test/controllers/admin/generator_controller_test.rb
+++ test/controllers/admin/generator_controller_test.rb
@@ -23,14 +23,14 @@ class Admin::GeneratorControllerTest < ActionDispatch::IntegrationTest
       post admin_generator_index_url, params: {generator: {users: "", accounts: 2, transactions: 3}}
 
       #should redirect to same form page if there is an error
-      assert_generates "admin/generator/new", controller: "generator", action: "new"
+      assert_generates "admin/generator/new", controller: "admin/generator", action: "new"
     end
 
     test 'admin cannot generate fake data with no accounts input' do
       post admin_generator_index_url, params: {generator: {users: 1, accounts: "", transactions: 3}}
 
       #should redirect to same form page if there is an error
-      assert_generates "admin/generator/new", controller: "generator", action: "new"
+      assert_generates "admin/generator/new", controller: "admin/generator", action: "new"
     end
 
 end
